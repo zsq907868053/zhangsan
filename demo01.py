fd9dae43d9d5c8fed4c5291e02882ec4b84c423b
Complicated <zsq907868053@gmail.com>
2020-10-06 22:38:30 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,52 @@
+"""
+print("hello world!") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+
+我见青山多妩媚
+料青山见我应如是
+
+print("人生若只如初见","何事秋风悲画扇",777)
+print("哈哈"+"嘻嘻") #字符串的拼接
+print("嘿嘿"*7)
+print(233+233)
+print(2>3)
+
+#变量(给某个值取名)
+#赋值(把具体的某个值交给变量)
+a = "张三" #把 张三 这个值 赋值给名字为a的这个变量 变量必须为小写字母
+print(a)
+
+a = input("请输入: ")
+b = input("请输入: ")
+print("input输入的内容 ",a+b)
+
+#数据格式的转换
+c = type(2333)
+print(c)
+
+"""
+
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a = float(input("请输入"))
+# b = float(input("请输入"))
+# print("input获取的内容:",a+b)
+
+
+# a = 'sadasdasdasdasdadasdasdadadas   sadasda'
+# print(len(a))
+
+a = input("请输入：")
+b = input("请输入：")
+print("字数",len(a)+len(b))
